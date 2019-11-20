cfc543c84ac0bc82b54e1fe833a0577c76bedc65
Antoine Marandon <antoine@marandon.fr>
2019-11-20 14:31:20 +0900

    Initial Commit

---
+++ EurekaFirebaseAdventCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EurekaFirebaseAdventCalendar
+//
+//  Created by Antoine Marandon on 20/11/2019.
+//  Copyright © 2019 Antoine Marandon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
